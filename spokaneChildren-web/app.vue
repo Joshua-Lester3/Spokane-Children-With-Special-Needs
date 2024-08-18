b561e5c17236ed8c9bee5d08586676c2ef55c599
Joshua Lester <jlester3@ewu.edu>
2024-08-18 16:42:39 -0700

    App bar + nav drawer in app.vue

--- spokaneChildren-web/app.vue
+++ spokaneChildren-web/app.vue
@@ -1,6 +1,44 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <v-app-bar :elevation="2">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon @click.prevent.stop="showNavDrawer = true" />
+      </template>
+
+      <v-app-bar-title>Spokane Children with Special Needs</v-app-bar-title>
+    </v-app-bar>
+
+    <ClientOnly>
+      <v-navigation-drawer v-model="showNavDrawer" :width="navigationDrawerWidth" disable-resize-watcher temporary>
+        <v-list class="text-center">
+          <v-list-item>
+            Hello
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </ClientOnly>
+
+    <v-main>
+      <NuxtPage />
+    </v-main>
+  </v-app>
 </template>
+
+<script setup lang="ts">
+import { useDisplay } from 'vuetify';
+
+const display = ref(useDisplay());
+
+const showNavDrawer = ref(false);
+const navigationDrawerWidth = computed(() => {
+  switch (display.value.name) {
+    case 'xs': return 175
+    case 'sm': return 200;
+    case 'md': return 225;
+    case 'lg': return 250;
+    case 'xl': return 275;
+    case 'xxl': return 300;
+  }
+});
+
+</script>
--- todo.md
+++ todo.md
@@ -1,4 +1,1 @@
 ### todo:
-
-- split backend postAnnouncement up into edit/add. Single responsibility principle and all ;/
-- how to check for DateTime in Event being present in request body?
